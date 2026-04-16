29df053c533a7c34f5f71c6d6d4955414821e59d
Youssef AFify <youssef.afify122@gmail.com>
2026-04-16 22:39:43 +0200

    tmp4

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { passport } = require('./lib/passport');
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(passport.initialize())
 
